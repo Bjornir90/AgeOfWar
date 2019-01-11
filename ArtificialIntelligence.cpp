c7f6f173063b7f8a15aa9dd6ba7a86c76471905e
Bjornir90 <celestincollin@gmail.com>
2019-01-11 15:24:30 +0100

    Artificial Intelligence now properly buy units

--- ArtificialIntelligence.cpp
+++ ArtificialIntelligence.cpp
@@ -10,15 +10,18 @@
 
 Unit * ArtificialIntelligence::getNextBuy(){
     int currentMoney = this->getMoney();
+    Unit * toBuy;
     if(currentMoney >= 20){//buy catapult
-        Catapult catapult();
+        toBuy = new Catapult();
     } else if(currentMoney >= 12){//buy archer
-        Archer archer();
+        toBuy = new Archer();
     } else if(currentMoney >= 10){//buy soldier
-        Soldier soldier();
+        toBuy = new Soldier();
     } else {
         printf("Trolol tu es pauvre");
+        toBuy = nullptr;
     }
+    return toBuy;
 }
 
 ArtificialIntelligence::ArtificialIntelligence(int money, int team, int hp) : Player(money, team, hp) {}
