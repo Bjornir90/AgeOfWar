70788e26423807a1c6c0663b9a145e533c19c28a
Bjornir90 <celestincollin@gmail.com>
2019-01-26 18:42:47 +0100

    Add aliases for buying unit, and game should no longer crash when entering a non-existent unit name

--- Human.cpp
+++ Human.cpp
@@ -15,16 +15,19 @@ Unit * Human::getNextBuy() {
         std::cout<<"Pièces : "<< getMoney() <<"\r\nEntrez le nom de l'unité à acheter (nothing, catapult, soldier ou archer) :"<<std::endl;
         std::cin >> unitName;
         Unit *toBuy;
-        if (unitName == "catapult") {
+        if (unitName == "catapult" || unitName == "c") {
            toBuy = new Unit(&Catapult::instance, *this);
-        } else if (unitName == "archer") {
+        } else if (unitName == "archer" || unitName == "a") {
             toBuy = new Unit(&Archer::instance, *this);
-        } else if (unitName == "soldier") {
+        } else if (unitName == "soldier" || unitName == "s") {
             toBuy = new Unit(&Soldier::instance, *this);
         } else if (unitName == "superunicorn") {
             //Spawn a special unit
-        } else if (unitName == "nothing"){
+        } else if (unitName == "nothing" || unitName == "n"){
             return nullptr;
+        } else {
+            std::cout << "Ce nom n'est pas valide"<<std::endl;
+            continue;
         }
         if(toBuy->type->price <= this->getMoney()){
             this->spendMoney(toBuy->type->price);
